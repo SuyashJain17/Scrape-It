37302464b272327f9727d9777cc3cb3d8fbead12
Suyash Jain <suyashjain1707@gmail.com>
2025-04-27 03:39:15 +0530

    added features

--- components/BreadcrumHeader.tsx
+++ components/BreadcrumHeader.tsx
@@ -1,30 +1,38 @@
-"use client"
-import { usePathname } from 'next/navigation'
-import React from 'react'
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from './ui/breadcrumb';
+'use client';
+
+import React from 'react';
+import { usePathname } from 'next/navigation';
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
 import { MobileSidebar } from './Sidebar';
 
-function BreadcrumHeader () {
-  const pathName = usePathname();
-  const paths = pathName === "/" ? [""]: pathName.split("/");
-    return (
-    <div className='flex items-center flex-start'>
-        <MobileSidebar />
-        <Breadcrumb>
-            <BreadcrumbList>
-                {paths.map((path, index) => (
-                    <React.Fragment key={index}>
-                        <BreadcrumbItem>
-                            <BreadcrumbLink className='capitalize' href = {`${path}`}>
-                                {path === "" ? "home" : path} 
-                            </BreadcrumbLink>
-                        </BreadcrumbItem>
-                    </React.Fragment>
-                ))}
-            </BreadcrumbList>
-        </Breadcrumb>
-    </div>
-  )
-}
+export default function BreadcrumbHeader() {
+  const pathname = usePathname();
+  const paths = pathname === '/' ? [''] : pathname.split('/');
 
-export default BreadcrumHeader
+  return (
+    <div className="flex items-center flex-start">
+      <MobileSidebar />
+      <Breadcrumb>
+        <BreadcrumbList>
+          {paths.map((path, index) => (
+            <React.Fragment key={index}>
+              <BreadcrumbItem>
+                <BreadcrumbLink className="capitalize" href={`/${path}`}>
+                  {path === '' ? 'home' : path}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              {index !== paths.length - 1 && <BreadcrumbSeparator />}
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
--- lib/workflow/executor/registry.ts
+++ lib/workflow/executor/registry.ts
@@ -8,6 +8,9 @@ import { ExtractTextFromElementExecutor } from "./ExtractTextFromElementExecutor
 import { FillInputExecutor } from "./FillInputExecutor";
 import { ClickElementExecutor } from "./ClickElementExecutor";
 import { WaitForElementExecutor } from "./WaitForElementExecutor";
+import { DeliverViaWebhookExecutor } from "./DeliverViaWebhookExecutor";
+import { ExtractDataWithAiExecutor } from "./ExtractDataWithAiExecutor";
+import { ReadPropertyFromJsonExecutor } from "./ReadPropertyFromJsonExecutor";
 
 type ExecutorFn<T extends WorkflowTask> = (
     environment: ExecutionEnvironment<T>
@@ -23,5 +26,8 @@ export const ExecutorRegistry: RegistryType = {
     EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElementExecutor,
     FILL_INPUT: FillInputExecutor,
     CLICK_ELEMENT: ClickElementExecutor,
-    WAIT_FOR_ELEMENT: WaitForElementExecutor
-}
+    WAIT_FOR_ELEMENT: WaitForElementExecutor,
+    DELIVER_VIA_WEBHOOK: DeliverViaWebhookExecutor,
+    EXTRACT_DATA_WITH_AI: ExtractDataWithAiExecutor,
+    READ_PROPERTY_FROM_JSON: ReadPropertyFromJsonExecutor
+}   
